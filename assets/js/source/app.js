885651441055df67a3faa19e0940b6d7a271a024
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2018-01-29 10:34:55 +0800

    Initialize bootstrap-material-design

--- assets/js/source/app.js
+++ assets/js/source/app.js
@@ -1,5 +1,8 @@
 (function($){
 	$(document).ready(function(){
 		$('.gallery').find('br').detach();
+
+		// Initialize Bootstrap Material Design
+		$('body').bootstrapMaterialDesign();
 	});
 })(jQuery);
