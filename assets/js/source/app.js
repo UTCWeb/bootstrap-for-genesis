545cb2ecec2924aa04f73761573e4c2ab47e7cbd
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2018-04-03 16:59:22 +0800

    Added webfont, removed unneeded line of codes

--- assets/js/source/app.js
+++ assets/js/source/app.js
@@ -1,8 +1,24 @@
 (function($){
+	WebFontConfig = {
+		google: { families: [ 'Material+Icons' ] },
+		active: function() {
+			sessionStorage.fonts = true;
+		}
+	};
+	
+	var $source = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+	
+	(function() {
+		var wf = document.createElement('script');
+		wf.src = $source;
+		wf.type = 'text/javascript';
+		wf.async = 'true';
+	
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(wf, s);
+	})();
+
 	$(document).ready(function(){
 		$('.gallery').find('br').detach();
-
-		// Initialize Bootstrap Material Design
-		$('body').bootstrapMaterialDesign();
 	});
 })(jQuery);
